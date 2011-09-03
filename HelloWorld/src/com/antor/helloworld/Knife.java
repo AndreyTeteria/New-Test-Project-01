610b1a6dbf9886ec59e0c9de95f05d84a7ea2c58
Andrey Teteria <Paladin26@yandex.ru>
2011-09-03 19:16:46 +0300

    Added class which represents a knife.
    Change-Id: Ifb0770a19675a0a4f28e11ca0645a348a207e682
    Signed-off-by: Andrey Teteria <Paladin26@yandex.ru>

---
+++ HelloWorld/src/com/antor/helloworld/Knife.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.antor.helloworld;
+
+/**
+ * @author Andrey Teteria
+ * 
+ */
+public class Knife {
+
+	private double length_;
+	@Override
+	public String toString() {
+		return "Hello, I am Knife of " + length_ + " mm length!";
+	}
+
+}
